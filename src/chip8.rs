798a8c67e25a69899c3b5323770029f52ba4746b
Pyramidal <andrei.but06@proton.me>
2026-08-28 21:26:16 +0300

    Added Chip8 struct

---
+++ src/chip8.rs
@@ -0,0 +1,13 @@
+struct Chip8 {
+    registers: [u8; 16],
+    memory: [u8; 4096],
+    index: u16,
+    pc: u16,
+    stack: [u16; 16],
+    sp: u8,
+    delay_timer: u8,
+    sound_timer: u8,
+    keypad: [u8; 16],
+    video: [bool; 64 * 32],
+    opcode: u16,
+}
