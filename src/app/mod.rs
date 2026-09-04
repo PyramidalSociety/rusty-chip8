e26705bb1536df3340107315e2d4f7ddae9288b5
Pyramidal <andrei.but06@proton.me>
2026-09-04 14:20:26 +0300

    Added the App struct and implemented the new associated function

---
+++ src/app/mod.rs
@@ -0,0 +1,16 @@
+use crate::chip8::Chip8;
+use std::{io, path::Path};
+
+pub struct App {
+    chip8: Chip8,
+    exit: bool,
+}
+
+impl App {
+    pub fn new(pth: &Path) -> Result<Self, io::Error> {
+        Ok(Self {
+            chip8: Chip8::new(&pth)?,
+            exit: false,
+        })
+    }
+}
