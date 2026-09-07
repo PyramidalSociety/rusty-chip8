11af9466689ddf3804ca7fbb97e67f1dd018cb96
Pyramidal <andrei.but06@proton.me>
2026-09-07 18:06:21 +0300

    Fixed beeper bug with a minimum beep duration

--- src/app/beeper.rs
+++ src/app/beeper.rs
@@ -1,8 +1,13 @@
+use std::time::{Duration, Instant};
+
 use rodio::{DeviceSinkBuilder, MixerDeviceSink, Player, Source, source::SquareWave};
+
+const MIN_BEEP_DURATION: Duration = Duration::from_millis(75);
 
 pub(super) struct Beeper {
     _handle: MixerDeviceSink,
     player: Player,
+    sound_start: Option<Instant>,
 }
 
 impl Beeper {
@@ -13,19 +18,28 @@ impl Beeper {
 
         let source = SquareWave::new(440.0).repeat_infinite();
         player.append(source);
-        player.pause();
+        player.set_volume(0.0);
 
         Self {
             _handle: handle,
             player,
+            sound_start: None,
         }
     }
 
-    pub(super) fn play(&self) {
-        self.player.play();
+    pub(super) fn play(&mut self) {
+        if self.sound_start.is_none() {
+            self.sound_start = Some(Instant::now());
+            self.player.set_volume(0.5);
+        }
     }
 
-    pub(super) fn pause(&self) {
-        self.player.pause();
+    pub(super) fn pause(&mut self) {
+        if let Some(start_time) = self.sound_start {
+            if start_time.elapsed() >= MIN_BEEP_DURATION {
+                self.player.set_volume(0.0);
+                self.sound_start = None;
+            }
+        }
     }
 }
